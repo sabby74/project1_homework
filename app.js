985de6fe778042bde8a95ac5be7f1f64acf7bb67
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-01 19:38:47 -0400

    given some styles to css h1 and added an image

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 console.log("hello");
+// sending the request to get the data back to browser.
+const promise = $.ajax({
+  url: "https://akabab.github.io/superhero-api/api/all.json",
+});
+
+promise.then(
+  (data) => {
+    console.log(data);
+  },
+  (error) => {
+    console.log("bad request: ", error);
+  }
+);
+
+
 
