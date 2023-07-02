29d19f11b1bf34a60a2c3a177a127564fbe84249
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-01 20:24:36 -0400

    setting the dom

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 console.log("hello");
+
+const $name = $("#name");
+const $appearance = $("#appearance");
+const $biography = $("biography");
+const $powerstats = $("powerstats");
+
 // sending the request to get the data back to browser.
 const promise = $.ajax({
   url: "https://akabab.github.io/superhero-api/api/all.json",
@@ -12,6 +18,4 @@ promise.then(
     console.log("bad request: ", error);
   }
 );
-
-
 
