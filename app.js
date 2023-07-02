e921793602ec2d74a718ce7c33e7f18bb8c54ff3
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-01 21:26:24 -0400

    d

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 console.log("hello");
 
 
-const $name = $("#name");
-const $appearance = $("#appearance");
-const $biography = $("biography");
-const $powerstats = $("powerstats");
+// const $name = $("#name");
+// const $appearance = $("#appearance");
+// const $biography = $("biography");
+// const $powerstats = $("powerstats");
 
 
-// sending the request to get the data back to browser.
+
 const promise = $.ajax({
   url: "https://akabab.github.io/superhero-api/api/all.json",
 });
