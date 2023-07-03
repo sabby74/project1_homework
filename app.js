f1805866c60bef76f6b8d4710c3119d5e72c1ccd
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-02 21:10:10 -0400

    filter method to get each hero

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 console.log("hello");
 let $form = $("form");
 
-
-const renderHero= (hero)=>{
-    let $div = $(`<div> <h1> Name :${hero.name}</h1> </div>`);
-    $div.append(`Full Name: ${hero.biography.fullName}`)
-    $div.append(`<img src= '${hero.images.sm}' alt='${hero.name}'/>`);
-    $div.append(`<p> Intelligence: ${hero.powerstats.intelligence}  </p>`);
-    $div.append(`<p> Strength: ${hero.powerstats.strength}  </p>`);
-    $div.append(`<p> Speed: ${hero.powerstats.speed}  </p>`);
-   $("body").append($div);
-}
+const renderHero = (hero) => {
+  let $div = $(`<div> <h1> Name :${hero.name}</h1> </div>`);
+  $div.append(`Full Name: ${hero.biography.fullName}`);
+  $div.append(`<img src= '${hero.images.sm}' alt='${hero.name}'/>`);
+  $div.append(`<p> Intelligence: ${hero.powerstats.intelligence}  </p>`);
+  $div.append(`<p> Strength: ${hero.powerstats.strength}  </p>`);
+  $div.append(`<p> Speed: ${hero.powerstats.speed}  </p>`);
+  $("body").append($div);
+};
 
 const promise = $.ajax({
   url: "https://akabab.github.io/superhero-api/api/all.json",
@@ -20,7 +19,7 @@ promise.then(
   (data) => {
     console.log(data);
     data.map((hero) => {
-        renderHero(hero);
+      renderHero(hero);
     });
   },
   (error) => {
@@ -28,9 +27,30 @@ promise.then(
   }
 );
 
-$form.on("submit", (event)=>{
-event.preventDefault();
-let $input = $("input");
-console.log($input.val());
-});
+$form.on("submit", (event) => {
+  event.preventDefault();
+  let $input = $("input");
+  // console.log($input.val());
 
+  const promise = $.ajax({
+    url: "https://akabab.github.io/superhero-api/api/all.json",
+  });
+
+  promise.then(
+    (data) => {
+      //   console.log(data);
+
+      let myHero = data.filter((hero) => hero.name === $input.val());
+      console.log(myHero);
+      if (myHero) {
+
+        
+          myHero.map((hero) => {
+            renderHero(hero);
+          });
+
+      }
+    },
+ 
+  );
+});
